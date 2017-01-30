accff265e713f29b21c562c81f36ff506a8a0337
millicentwanjiku <milicentnjuguna0@gmail.com>
2017-01-30 20:03:45 +0300

    working on value of zero one two

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,17 @@
+"""A program calculates the fibonacci sequence of numbers"""
 def fibonacci(number):
+	"""Afunction that returns the fibonacci value of a number"""
 	if type(number) != int:
 		return "Only integers are allowed"
+	if number < 0:
+		return "Only positive numbers are accepted"
+	elif number == 0:
+		return 0
+	elif number == 1:
+		return 1
+	elif number == 2:
+		return 1
+		
+	
+
+
--- testfibonacci.py
+++ testfibonacci.py
@@ -16,3 +16,29 @@ class FibonacciTests(unittest.TestCase):
     def test_if_float_nuumbers_are_allowed(self):
     	"""Tests if dictionary inputs are allowed"""
     	self.assertEqual(fibonacci(52.28), 'Only integers are allowed')
+
+    def test_if_it_accepts_negative_numbers(self):
+    	"""Tests if the function accepts negative numbers"""
+    	self.assertEqual(fibonacci(-2), 'Only positive numbers are accepted')
+
+    def test_if_the_return_value_of_zero_is_zero(self):
+    	"""Tests if dictionary inputs are allowed"""
+    	self.assertEqual(fibonacci(0), 0)
+
+    def test_if_the_return_value_of_one_is_one(self):
+    	"""Tests if dictionary inputs are allowed"""
+    	self.assertEqual(fibonacci(1), 1)
+
+    def test_if_the_return_value_of_two_is_one(self):
+    	"""Tests if dictionary inputs are allowed"""
+    	self.assertEqual(fibonacci(2), 1)
+
+    def test_if_the_function_returns_correct_value(self):
+    	"""Tests if the function returns the correct value of a number"""
+    	self.assertEqual(fibonacci(6), 8)
+
+    def test_if_the_function_returns_result_of_type_integer(self):
+    	"""Tests if the function returns a value of type integer"""
+    	self.assertIsInstance(fibonacci(7), int)
+
+    
